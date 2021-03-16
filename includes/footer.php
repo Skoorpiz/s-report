67385abe942ea8c04fd876f6420a9cdcfeed68e2
Skoorpiz <webhostsio2@gmail.com>
2021-03-16 11:56:02 +0100

    change language datepicker

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,20 @@
 </body>
 </html>
 <script>
+    jQuery(function($){
+        $.datepicker.regional['fr'] = {
+            monthNames: ['Janvier','Fevrier','Mars','Avril','Mai','Juin',
+                'Juillet','Aout','Septembre','Octobre','Novembre','Decembre'],
+            monthNamesShort: ['Jan','Fev','Mar','Avr','Mai','Jun',
+                'Jul','Aou','Sep','Oct','Nov','Dec'],
+            dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+            dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
+            dayNamesMin: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
+            dateFormat: 'dd-mm-yy',
+            firstDay: 1,
+        };
+        $.datepicker.setDefaults($.datepicker.regional['fr']);
+    });
     $(function () {
         $(".datepicker").datepicker({
             dateFormat: 'dd/mm/yy'
